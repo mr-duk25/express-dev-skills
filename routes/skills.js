3c4269046dfcee23f584f0a7a645ad32a5911927
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-05-02 19:56:56 -0700

    + 'show.ejs', links to home and all skills

--- routes/skills.js
+++ routes/skills.js
@@ -6,6 +6,9 @@ var skillsCtrl = require('../controllers/skills')
 //GET /skills
 router.get('/', skillsCtrl.index)
 
+//GET /skills:id
+router.get('/:id', skillsCtrl.show);
+
 
 module.exports = router;
 
