da2d00a0b738e27ec2f9fe1aac7719a3e0067f75
mr-duk25 <samuelrobles@Samuels-MacBook-Pro.local>
2023-05-02 19:13:41 -0700

    require 'Skill' model

--- routes/skills.js
+++ routes/skills.js
@@ -3,4 +3,10 @@ var router = express.Router();
 var skillsCtrl = require('../controllers/skills')
 // All actual paths start with "/skills"
 
+//GET /skills
+router.get('/', skillsCtrl.index)
+
+
 module.exports = router;
+
+
